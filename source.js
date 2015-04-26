2a255ef708b289ee9f58b94a0db76b99492f95b9
Jon Lagrew <lagrewj@onid.oregonstate.edu>
2015-04-26 11:44:46 -0700

    updated final request functions

--- source.js
+++ source.js
@@ -11,101 +11,98 @@
 //Display Results
 //clear results
 
-//function is the handler function to be called when the window's load event fires
-//Global Event Handler
+//global call to refresh favorites 
+//global even handler
 window.onload = function()
 {
-	displayFavorites();
+	redisplaymyFavorites();
 }
-
-//clear favorites function will clear all favorites with empty string
-function clearFavorites()
+//function to clear favorites
+//does so by making string empty 
+function clearmyFavorites()
 {
-	//setting html to empty string which will clear the favorites
-	//The innerHTML property sets or returns the HTML content (inner HTML) of an element
 	document.getElementById("favoritesList").innerHTML = "";
 
 }
-
-function displayFavorites()
+function redisplaymyFavorites()
 {
-	//clearing favorites
-	clearFavorites();
+	//clear
+	clearmyFavorites();
 
-	//if local storage favorites exists then strings will get called by AJAX to query GitHub 
-	if (localStorage.getItem("favorites")) 
+	//if local storage favorites exists then strings will get called by AJAX to query GitHub
+	if (localStorage.getItem("favGists")) 
 	{
-		var favorite_string = localStorage.getItem("favorites");
-		//splitting by comma
-		var favorite_list = favorite_string.split(",");
-		//for loop running through favorites list length
-		for(var i =0;i<favorite_list.length;i++)			  	
+		var favoriteString = localStorage.getItem("favGists");
+		var favoriteList = favoriteString.split(",");
+		for(var i =0;i<favoriteList.length;i++)
 		{
-			getGist(favorite_list[i]);
+			oneGist(favoriteList[i]);
 		}
 	};
-}
-//adding favorites function first check gistID list 
-//then if enough storage locally will add to string or add just ID
-function addFavorite(gistID)
-{
-	if (!locatedInFavorites(gistID))
-	{
-		//checking local storage of favorites
-		if (localStorage.getItem("favorites"))
-		{
-			var favorites = localStorage.getItem("favorites");
-			favorites = favorites+","+gistID;
-			if(!locatedInFavorites(gistID))
-			{
-				localStorage.setItem("favorites", favorites);
-
-			}
-		}
-		else
-		{
-			localStorage.setItem("favorites", gistID);
-		}
-
-		// Reconstruct the favorites list and the requested gists
-		displayFavorites();
-		getGists();
-	}
 }
 //remove will check if gistID is in favorites list and loop through
 //list by comma again and insert into local storage 
 //redisplay favorites
-function removeFavorite(favorite_gistID)
+function deleteFromFavList(favoriteID)
 {
-	var gistID = favorite_gistID.split("fav-")[1];
-	if(locatedInFavorites(gistID))
+	
+	var gistID = favoriteID.split("fav-")[1];
+	if(isInFavList(gistID))
 	{
-		var newfavorites = []
-		var favorites = localStorage.getItem("favorites");
-		var favoriteList = favorites.split(",");
+		var newmyFavorites = []
+		var myFavorites = localStorage.getItem("favGists");
+		var favoriteList = myFavorites.split(",");
 		for(i=0;i<favoriteList.length;i++)
 		{
 			gist = favoriteList[i];
 			if(gist != gistID )
 			{
-				newfavorites.push(gist);
+				newmyFavorites.push(gist);
 			}
 		}
-		localStorage.setItem("favorites",newfavorites.join());
-		displayFavorites();
+		localStorage.setItem("favGists",newmyFavorites.join());
+		redisplaymyFavorites();
 	}
 }
+//adding favorites function first check gistID list 
+//then if enough storage locally will add to string or add just ID
+function addToFavList(gistID)
+{
+	if (!isInFavList(gistID))
+	{
+		//checking local storage of favorites
+		if (localStorage.getItem("favGists"))
+		{
+			var myFavorites = localStorage.getItem("favGists");
+			myFavorites = myFavorites+","+gistID;
+			if(!isInFavList(gistID))
+			{
+				localStorage.setItem("favGists", myFavorites);
 
+			}
+		}
+		else
+		{
+			localStorage.setItem("favGists", gistID);
+		}
+
+		//redisplaying new list then will call searchGists
+		redisplaymyFavorites();
+		searchGists();
+	}
+}
 //this helper function will be used in add and remove favorites list to check to see if its 
 //already in favorites list by comparing IDs
-function locatedInFavorites(gistID)
+function isInFavList(gistID)
 {
-	if (localStorage.getItem("favorites"))
+	//local storage favs
+	if (localStorage.getItem("favGists"))
 	{
-		var favorites = localStorage.getItem("favorites");
-		var favoriteList = favorites.split(",");
+		var myFavorites = localStorage.getItem("favGists");
+		var favoriteList = myFavorites.split(",");
 		for(i=0;i<favoriteList.length;i++)
 		{
+			//return true if in list already
 			gist = favoriteList[i];
 			if(gist == gistID )
 			{
@@ -118,51 +115,48 @@ function locatedInFavorites(gistID)
 }
 //this will filter the language search results to only show checked languages
 //default is set to show all results
-function filterGist(pythonCheck, SQLCheck, JSONCheck, JSCheck)
+function filterGist(gist, pychk, sqlchk, jsonchk, javachk)
 {
-//will go through each checked language if checked
+	//for each language pushing that language on the stack
 	var checkedLanguage = []
-	if(pythonCheck)
+	if(pychk)
 	{
 		checkedLanguage.push("python");
 	}
-	if(SQLCheck)
+	if(sqlchk)
 	{
 		checkedLanguage.push("sql");
 	}
 
-	if(JSCheck)
+	if(javachk)
 	{
 		checkedLanguage.push("javascript");
 	}
-	if(JSONCheck)
+	if(jsonchk)
 	{
 		checkedLanguage.push("json");
 	}
 
-	//default will show all results set to true 
+	//default all languages searched
 	if (checkedLanguage.length == 0)
 	{
 		return true;
 	}
-	//returning true if match in files
 	var files = gist.files;
-	//nested for loop
-	for (var chosen in files)
+	for (var article in files)
 	{
-		//comparing dictionary
-		var dictionary = files[chosen];
-		for(var item in dictionary)
+		
+		var dict = files[article];
+		for(var item in dict)
 		{
 			if(item == "language")
 			{
-				//going through checked language will return true
-				gistLanguage = dictionary.language;
+				gist_lang = dict.language;
 				for(var i = 0; i<checkedLanguage.length;i++)
 				{	
-					if(gistLanguage != null)
+					if(gist_lang != null)
 					{	
-						if(gistLanguage.toLowerCase() == checkedLanguage[i])
+						if(gist_lang.toLowerCase() == checkedLanguage[i])
 						{
 							return true;
 						}
@@ -172,24 +166,23 @@ function filterGist(pythonCheck, SQLCheck, JSONCheck, JSCheck)
 			}
 		}
 
-		
 	}
 	return false;
-}	
+}
 //will return language if in gist objects
-//called in displayGists function
+//called in display function
 function getLanguage(gist)
 {
-	
+	//goes through object
 	var files = gist.files;
-	for (var chosen in files)
+	for (var article in files)
 	{
-		var dictionary = files[chosen];
-		for(var item in dictionary)
+		var dict = files[article];
+		for(var item in dict)
 		{
 			if(item == "language")
 			{
-				return dictionary.language;
+				return dict.language;
 			}
 		}
 	}
@@ -197,7 +190,8 @@ function getLanguage(gist)
 }
 
 function displayGists(gists)
-{	//searchResults div 
+{	
+	//searchResults div
 	var main = document.getElementById("searchResults");
 
 	//the 4 requested languages from the checked boxes
@@ -212,37 +206,157 @@ function displayGists(gists)
 	// Create and html string and inject it into the "searchResults"
 	for(var i = 0; i<gists.length; i++)
 	{
-		var gistObject =  JSON.stringify(gists[i]);
-		gistObject = gists[i];
-		var files = gistObject.files;
-		var gistID = gistObject['id'];
-	//filter if not in favorites but located in gists filter function 
-		if (!inFavorites(gistID) && filterGist(gistObject, python,sql,json,javascript))
+		var objectGist =  JSON.stringify(gists[i]);
+		objectGist = gists[i];
+		var files = objectGist.files;
+		var gistID = objectGist['id'];
+		//filter if not in favorites but located in gists filter function
+		if (filterGist(objectGist, python,sql,json,javascript) && !isInFavList(gistID))
 		{	
 			console.log("displaying");
-			var gist_owner = gistObject.owner;
-			var gist_created = gistObject.created_at;
-			var gist_desc = gistObject.description;
-			var gist_repo = gistObject.html_url;
-			var id = "<b>id: </b><a href="+gist_repo+">" + gistID +"</a><br>";
-			var description = "<b>Description: </b>" + gist_desc +"<br>";
-			var language = "<b>Language: </b>" + getLanguage(gistObject) +"<br>";
-			//adding favorites button on click
-			var button =  '<button id="'+gistID+'" onclick="addFavorite(this.id)">Add to Favorites</button>';
-			var gisthtml = "<div class='gist-item'>" + id + description + language + button +"<br></div>";	
-			var element = document.creatElement("div");
-			element.id = "div-"+gistID;
-			element.innerHTML = gisthtml;
-			//method to add node to end of list of children 
-			main.appendChild(element);
+			//creating variables for displaying search results
+			var gist_owner = objectGist.owner;
+			var gist_created = objectGist.created_at;
+			var gist_desc = objectGist.description;
+			var gist_repo = objectGist.html_url;
+			//ensuring spaces and breaks to display nicely
+			var idhtml = "<b>id: </b><a href="+ gist_repo +">" + gistID +"</a><br>";
+			var deschtml = "<b>Description: </b>" + gist_desc +"<br>";
+			var langhtml = "<b>Language: </b>" + getLanguage(objectGist) +"<br>";
+			var buttonClick =  '<button id="'+ gistID +'" onclick="addToFavList(this.id)">Add to Favorites</button>';
+			var gisthtml = "<div class='gist-item'>" + idhtml + deschtml +langhtml + buttonClick +"<br></div>";	
+			var elem = document.createElement("div");
+			elem.id = "div-"+gistID;
+			//inner html method
+			elem.innerHTML = gisthtml;
+			main.appendChild(elem);
 		}
 	} 
 }
-
-//clear function to clear search results each search
+//clearing searchresults before rebuild
 function clear()
 {	
-	var refresh = document.getElementById("searchResults");
-	refresh.innerHTML = "";
+	var mainDiv = document.getElementById("searchResults");
+	mainDiv.innerHTML = "";
 
 }
+//this function will clear, get search results, get number of pages and request each page
+//if not between 1 through 5 will display an error to the user 
+function searchGists()
+{	
+	
+	clear();
+	var main = document.getElementById("searchResults");
+	var numberOfPages = document.getElementById("selectedPages").value;
+	var totalGists = [];
+	if(numberOfPages >=1 && numberOfPages <=5)
+	{
+		var header = document.createElement("h2");
+		header.innerHTML = "Search Results:<br><br>";
+		main.appendChild(header);
+		//from 1 to number of pages
+		for(i=1; i<=numberOfPages; i++)
+		{
+			requestGists(i);
+		}
+	}
+	//error to user
+	else
+	{
+		var header = document.createElement("h2");
+		header.innerHTML = "Please enter page between 1 and 5.<br><br>";
+		main.appendChild(header);
+	}
+}
+
+//creates AJAX request for id
+function oneGist(gistID)
+{
+	//ajax request
+	var ajaxRequest = new XMLHttpRequest();
+	//url and id 
+	var gisturl = "https://api.github.com/gists/"+gistID;
+	//getting url
+	ajaxRequest.open('GET',gisturl);
+	//send null
+	ajaxRequest.send(null);
+		ajaxRequest.onreadystatechange = function()
+		{
+			if(ajaxRequest.readyState == 4)
+			{
+				if(ajaxRequest.status == 200)
+				{
+					console.log("good request");
+					var response = JSON.parse(ajaxRequest.responseText);
+					insertFavorite(response);
+					return response;
+				}
+				else
+				{
+					console.log("bad request");
+				}
+			}
+		};
+}
+//inserting favorites 
+function insertFavorite(response)
+{
+		var main = document.getElementById("favoritesList");
+
+		var gistID = response['id'];
+		var gist_owner = response.owner;
+		var gist_created = response.created_at;
+		var gist_desc = response.description;
+		var gist_repo = response.html_url;
+		var idhtml = "<b>id: </b><a href="+gist_repo+">" + gistID +"</a><br>";
+		var deschtml = "<b>Description: </b>" + gist_desc +"<br>";
+		var buttonClick =  '<button id="fav-'+gistID+'" onclick="deleteFromFavList(this.id)">Remove Favorite</button>';		
+		var gisthtml = "<div class='favorite-item'>" + idhtml + deschtml +buttonClick+"<br></div>";	
+		var elem = document.createElement("div");
+		elem.id = "div-fav-"+gistID;
+		elem.class = "favorite-item";
+		elem.innerHTML = gisthtml;
+			
+
+		main.appendChild(elem);
+
+}
+//AJAX request for gists
+function requestGists(page)
+{		
+		var ajaxRequest = new XMLHttpRequest();
+		var gisturl = "https://api.github.com/gists?page="+page;
+		console.log(gisturl);
+		ajaxRequest.open('GET',gisturl);
+		ajaxRequest.send(null);
+		ajaxRequest.onreadystatechange = function()
+		{
+			if(ajaxRequest.readyState == 4)
+			{
+				if(ajaxRequest.status == 200)
+				{
+					console.log("good request"  );
+					var response = JSON.parse(ajaxRequest.responseText);
+					displayGists(response);
+					return response;
+				}
+				else
+				{
+					console.log("bad request");
+				}
+			}
+		};
+}
+
+//resources: 
+//1. https://developer.mozilla.org/en-US/
+//2. http://www.sanwebe.com/2013/03/ajax-pagination-with-jquery-php
+//3. http://www.infotuts.com/ajax-pagination-mysql-php-and-jquery/
+//4. http://www.w3schools.com/ajax/
+//5. http://www.yourhtmlsource.com/javascript/ajax.html
+//6. http://api.jquery.com/jquery.ajax/
+//7. http://stackoverflow.com/questions/9154026/jquery-dynamically-load-a-gist-embed
+
+//known errors
+//1. had C# results returned 
+//2. html returns when js checked
